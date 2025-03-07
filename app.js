5c58dcb1371a4c5d1bbe92838cc164d9896e8bac
GellanX <angelfubol@yahoo.es>
2025-03-07 01:26:56 -0300

    Se añade la función para sortear los nombres de los amigos

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function agregarAmigo() {
     //Si el valor no es vacio, se agrega a la lista
     if (nombre !== "") {
         nombrePersonas.push(nombre);
+        //Cuando se agrega el nombre, se muestra en pantalla el nombre del amigo
         mostrarAmigos();
         document.getElementById("amigo").value = "";
     } else {
@@ -16,14 +17,27 @@ function agregarAmigo() {
     }
 }
 
-//Función para mostrar los amigos agregados
+//Función para mostrar los nombres de los amigos
 function mostrarAmigos() {
     let listaNombres = document.getElementById("listaAmigos");
     listaNombres.innerHTML = "";
 
     for (let i = 0; i < nombrePersonas.length; i++) {
         let item = document.createElement("li");
+        //El contador parte desde el inicio de la lista, así que siempre da con el nombre que se agrega
+        //El contador parte en cero, cuando la lista se haga más grande, el contador aumentará
         item.textContent = nombrePersonas[i];
         listaNombres.appendChild(item);
+    }
+}
+
+//Función para hacer el sorteo de los nombres de los amigos
+function sortearAmigo() {
+    if (nombrePersonas.length > 0) {
+        let numeroGanador = Math.floor(Math.random()*nombrePersonas.length);
+        let amigoGanador = nombrePersonas[numeroGanador];
+        document.getElementById("resultado").textContent = `El ganador es: ${amigoGanador}`;
+    } else {
+        alert("No hay nombres para sortear");
     }
 }
