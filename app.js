8db35c39af459395274ea300c170f2549b6ff1ef
GellanX <angelfubol@yahoo.es>
2025-03-07 00:28:15 -0300

    Se agrega lista de nombres y función para agregar nombre de amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Lista para guardar nombres
+let nombrePersonas = [];
+
+//Función para agregar amigos a la lista
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+
+    //Si el valor no es vacio, se agrega a la lista
+    if (nombre !== "") {
+        nombrePersonas.push(nombre);
+        document.getElementById("amigo").value = "";
+    } else {
+        alert("Por favor, ingresa un nombre.");
+    }
+}
